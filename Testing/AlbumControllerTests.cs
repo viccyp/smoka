b5a4c23cafaa051ede04b817fe33c30d20d394b8
viccyp <vic-vlp@outlook.com>
2026-06-15 12:08:01 +0100

    Service and controller tests rename

--- Testing/AlbumControllerTests.cs
+++ Testing/AlbumControllerTests.cs
@@ -2,5 +2,14 @@ namespace Testing;
 
 public class AlbumControllerTests
 {
-    
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
 }
--- Testing/AlbumServiceTests.cs
+++ Testing/AlbumServiceTests.cs
@@ -2,5 +2,14 @@ namespace Testing;
 
 public class AlbumServiceTests
 {
-    
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
 }
