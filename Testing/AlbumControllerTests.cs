b0adcb0418d8f263655b980b889e7e013e228721
viccyp <vic-vlp@outlook.com>
2026-06-15 16:28:37 +0100

    Album controller - GetAllAlbums with basic test

--- Testing/AlbumControllerTests.cs
+++ Testing/AlbumControllerTests.cs
@@ -1,15 +1,46 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using Moq;
+using Smoka.Controllers;
+using Smoka.Services;
+using Smoka.Models;
+
 namespace Testing;
 
 public class AlbumControllerTests
 {
+    private Mock<IAlbumService> _albumServiceMock;
+    private List<Album> _albums;
+    private List<Album> _emptyAlbums;
+    private AlbumController _albumController;
+
     [SetUp]
     public void Setup()
     {
+        _albumServiceMock = new Mock<IAlbumService>();
+        _albumController = new AlbumController(_albumServiceMock!.Object);
+
+        _albums = new List<Album>
+        {
+            new Album { Id = 1, Title = "Led Zeppelin", Genre = "Rock", ReleaseDate = 1971 },
+            new Album { Id = 4, Title = "Siembra", Genre = "Salsa", ReleaseDate = 1978 }
+        };
+
+        _emptyAlbums = new List<Album>();
+
     }
 
     [Test]
-    public void Test1()
+    public void GetAllAlbums_ReturnsFullListOfAlbums()
     {
-        Assert.Pass();
+        _albumServiceMock.Setup(service => service.GetAllAlbums()).Returns(_albums);
+
+        var result = _albumController.GetAllAlbums();
+
+        Assert.That(result, Is.TypeOf<OkObjectResult>());
+
+        var okResult = (OkObjectResult)result;
+
+        Assert.That(okResult.Value, Is.EqualTo(_albums));
     }
 }
--- src/Smoka/1-Controllers/AlbumController.cs
+++ src/Smoka/1-Controllers/AlbumController.cs
@@ -1,11 +1,23 @@
 using Smoka.Services;
+using Microsoft.AspNetCore.Mvc;
+using Smoka.Models;
 
 namespace Smoka.Controllers
 {
-    public class AlbumController(IAlbumService albumService) : IAlbumController
+    [ApiController]
+
+    public class AlbumController(IAlbumService albumService) : ControllerBase
     {
         private readonly IAlbumService _albumService = albumService;
+
+        [HttpGet]
+        public IActionResult GetAllAlbums()
+        {
+            var albums = _albumService.GetAllAlbums();
+            return Ok(albums);
+        }
     }
 
-    public interface IAlbumController;
+
+
 }
